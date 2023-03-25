3d94c9da3bda96c5150318f05f75553564a86d67
ssbbpeople <mcserverfeed321@gmail.com>
2023-03-25 16:51:50 -0500

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,41 @@
-#include "engine.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include <iostream>
 
 int main()
 {
-    Engine engine(640, 480, "OpenGL preset by Guylian Gilsing");
+	if (glfwInit() == 0) {
+		std::cerr << "Failed to initialize GLFW.\n";
+		return 1;
+	}
 
-    if(!engine.Initialize())
-    {
-        std::cout << std::endl << "Press any key to close program..." << std::endl;
-        std::cin.get();
-    }
+	auto* win = glfwCreateWindow(800, 600, "BasicGL", nullptr, nullptr);
+
+	if (win == nullptr) {
+		std::cerr << "Failed to create a window.\n";
+		glfwTerminate();
+		return 2;
+	}
+
+	glfwMakeContextCurrent(win);
+
+	if (gladLoadGL() == 0) {
+		std::cerr << "Failed to initlaize GLAD.\n";
+		glfwDestroyWindow(win);
+		glfwTerminate();
+		return 3;
+	}
+
+	glClearColor(.8F, .8F, .8F, 1.0F);
+
+	while (!glfwWindowShouldClose(win)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+		glfwSwapBuffers(win);
+	}
+
+	glfwDestroyWindow(win);
+	glfwTerminate();
 }
-
